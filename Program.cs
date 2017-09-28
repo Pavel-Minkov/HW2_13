5dfefb83f50b3ea733dc81b035cf0592e5a8b6dc
Pavel Minkov <piminkov@yahoo.com>
2017-09-28 23:12:58 +0300

    Adding the exercise

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameofNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int magic = int.Parse(Console.ReadLine());
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int count = 0;
+            bool found = false;
+            for (int i = n; i <= m; i++)
+            {
+                for (int j = n; j <= m; j++)
+                {
+                    if (i + j == magic)
+                    {
+                        firstNumber = i;
+                        secondNumber = j;
+                        found = true;
+                    }
+                    count++;
+                }
+            }
+            if (found)
+            {
+                Console.WriteLine($"Number found! {firstNumber} + {secondNumber} = {magic}");
+            } else
+            {
+                Console.WriteLine($"{count} combinations - neither equals {magic}");
+            }
+        }
+    }
+}
